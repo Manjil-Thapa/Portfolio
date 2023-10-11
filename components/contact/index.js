5c1c9c9afc3925d31a5e9b010e667038a50ea2c8
Manjil Thapa <manjil.thapa.mgr@gmail.com>
2023-10-11 23:52:56 +1000

    added basic styling on form layout

--- components/contact/index.js
+++ components/contact/index.js
@@ -24,53 +24,69 @@ export default function ContactForm() {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Name</label>
-        <input
-          type='text'
-          id='name'
-          name='name'
-          value={name}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor='email'>Email</label>
-        <input
-          type='email'
-          id='email'
-          name='email'
-          value={email}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor='subject'>Subject</label>
-        <input
-          type='text'
-          id='subject'
-          name='subject'
-          value={subject}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor='message'>Message</label>
-        <textarea
-          id='message'
-          rows='5'
-          name='message'
-          value={message}
-          onChange={handleChange}
-          required
-        ></textarea>
-      </div>
+    <div className='w-full max-w-xs'>
+      <form
+        onSubmit={handleSubmit}
+        className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'
+      >
+        <div>
+          <label
+            htmlFor='name'
+            className='block text-gray-700 text-sm font-bold mb-2'
+          >
+            Name
+          </label>
+          <input
+            className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+            type='text'
+            id='name'
+            name='name'
+            value={name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label
+            htmlFor='email'
+            className='block text-gray-700 text-sm font-bold mb-2'
+          >
+            Email
+          </label>
+          <input
+            type='email'
+            id='email'
+            name='email'
+            value={email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor='subject'>Subject</label>
+          <input
+            type='text'
+            id='subject'
+            name='subject'
+            value={subject}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor='message'>Message</label>
+          <textarea
+            id='message'
+            rows='5'
+            name='message'
+            value={message}
+            onChange={handleChange}
+            required
+          ></textarea>
+        </div>
 
-      <button>Send</button>
-    </form>
+        <button>Send</button>
+      </form>
+    </div>
   );
 }
