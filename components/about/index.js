82c326ba7546bd35cbe5fd1adc4a56f7cb4b6a5c
Manjil Thapa <manjil.thapa.mgr@gmail.com>
2023-10-12 23:02:46 +1000

    added about component

---
+++ components/about/index.js
@@ -0,0 +1,15 @@
+export default function AboutMe() {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <p>
+        {`Hello! I'm Manjil, a passionnate motivated self-taught programmer. I
+        like creating clean, responsive and functional web applications. My
+        focus is in creating responsive layouts on the JavaScript stack of React
+        and NodeJS. Highly driven and well-versed in RESTful architecture. I am
+        constantly seeking opportuinities to learn new concepts, technologies
+        and improve my knowledge.`}
+      </p>
+    </section>
+  );
+}
