c6e439b29969807b4a3a2b0dcdea56058d41fafa
Manjil Thapa <manjil.thapa.mgr@gmail.com>
2024-01-19 23:37:27 +1000

    added navbar boilerplate

---
+++ components/navbar/index.js
@@ -0,0 +1,25 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <p>
+          <a>Manjil</a>
+        </p>
+      </div>
+      <ul>
+        <li>
+          <a>Home</a>
+        </li>
+        <li>
+          <a>Projects</a>
+        </li>
+        <li>
+          <a>About</a>
+        </li>
+        <li>
+          <a>Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
