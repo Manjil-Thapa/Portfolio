78ada8683d8eabc02129421fc1ebd7a69d938f4c
Manjil Thapa <manjil.thapa.mgr@gmail.com>
2024-01-20 23:55:52 +1000

    added project component boilerplate

--- components/projects/index.js
+++ components/projects/index.js
@@ -1,3 +1,13 @@
+import Card from '../card';
+
 export default function Project() {
-  return <div>Project</div>;
+  return (
+    <section>
+      <h2>PROJECTS</h2>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </section>
+  );
 }
