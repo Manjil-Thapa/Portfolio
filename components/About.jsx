823e8c3eac227104a1bbc145a0ce37ae770ccd04
Manjil Thapa <manjil.thapa.mgr@gmail.com>
2024-01-29 23:26:53 +1000

    added credits on footer and react icons for about section

--- components/About.jsx
+++ components/About.jsx
@@ -1,3 +1,20 @@
+import { TbBrandNextjs } from 'react-icons/tb';
+import {
+  FaHtml5,
+  FaReact,
+  FaGitAlt,
+  FaCss3Alt,
+  FaNodeJs,
+  FaGithub,
+} from 'react-icons/fa';
+import {
+  SiMongodb,
+  SiJest,
+  SiTailwindcss,
+  SiExpress,
+  SiJavascript,
+} from 'react-icons/si';
+
 const About = () => {
   return (
     <section className='h-screen p-20'>
@@ -20,9 +37,12 @@ const About = () => {
         </div>
         <div>
           <h3>SKILLS & LANGUAGES</h3>
-          <ul className='flex gap-2'>
+          <ul className='flex gap-5'>
             {skills.map(skill => (
-              <li key={skill}>{skill}</li>
+              <li key={skill.name} className='w-[85px]'>
+                {skill.icon}
+                <span>{skill.name}</span>
+              </li>
             ))}
           </ul>
         </div>
@@ -34,14 +54,52 @@ const About = () => {
 export default About;
 
 const skills = [
-  'HTML',
-  'CSS',
-  'JavaScript',
-  'React',
-  'Git',
-  'Github',
-  'NextJS',
-  'MongoDB',
-  'Jest',
-  'TailwindCSS',
+  {
+    icon: <FaHtml5 size={40} color='#f06529' />,
+    name: 'HTML',
+  },
+  {
+    icon: <FaCss3Alt size={40} color='#2965f1' />,
+    name: 'CSS',
+  },
+  {
+    icon: <SiJavascript size={40} color='#f0db4f' />,
+    name: 'JavaScript',
+  },
+  {
+    icon: <FaReact size={40} color='#88dded' />,
+    name: 'React',
+  },
+  {
+    icon: <TbBrandNextjs size={40} />,
+    name: 'NextJS',
+  },
+  {
+    icon: <SiTailwindcss size={40} color='#fd5af8' />,
+    name: 'TailwindCSS',
+  },
+  {
+    icon: <SiExpress size={40} />,
+    name: 'ExpressJS',
+  },
+  {
+    icon: <FaNodeJs size={40} color='#68a063' />,
+    name: 'NodeJS',
+  },
+  {
+    icon: <SiMongodb size={40} color='#3fa037' />,
+    name: 'MongoDB',
+  },
+  {
+    icon: <SiJest size={40} color='#ffce00' />,
+    name: 'Jest',
+  },
+  {
+    icon: <FaGithub size={40} color='#24292e' fill='white' />,
+    name: 'Github',
+  },
+  {
+    icon: <FaGitAlt size={40} color='#f1502f' />,
+    name: 'Git',
+  },
 ];
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,19 @@
+import Link from 'next/link';
 import { PiCopyrightThin } from 'react-icons/pi';
 
 const Footer = () => {
   return (
     <footer className='flex flex-col justify-center items-center p-4'>
-      <h4>Designed & built by Manjil Thapa</h4>
+      <h4>
+        Designed & built by
+        <Link
+          href='https://github.com/Manjil-Thapa'
+          target='_blank'
+          className='ml-2'
+        >
+          Manjil Thapa
+        </Link>
+      </h4>
       <div className='flex justify-center items-center gap-1'>
         <PiCopyrightThin /> <h4>2024</h4>
       </div>
