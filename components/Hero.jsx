c97bb1949c6e1aecde144cef6ec6a493e03a8935
Manjil Thapa <manjil.thapa.mgr@gmail.com>
2024-02-06 16:55:26 +1000

    added framer motion animation

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,24 +1,81 @@
 import Image from 'next/image';
+import { motion } from 'framer-motion';
 
 const Hero = () => {
   return (
-    <section className='grid grid-cols-2 max-w-screen-xl m-auto h-5/6'>
-      <div>
-        <h1 className='scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0 max-w-max'>
-          Hi, I&apos;m Josh. A Frontend Developer based in Brisbane.
-        </h1>
-        <p>
-          I like creating clean, responsive and functional web apps with great user
-          experiences.
-        </p>
+    <section
+      className='grid grid-cols-2 max-w-screen-2xl m-auto p-20 h-screen'
+      id='home'
+    >
+      <div className='flex flex-col justify-center gap-2 pl-6'>
+        <motion.h2
+          initial={{ opacity: 0, x: -50 }}
+          animate={{
+            opacity: 1,
+            x: 0,
+            type: 'tween',
+            transition: { duration: 1, delay: 0.2 },
+          }}
+          className='text-2xl font-medium'
+        >
+          Hello there! I am
+        </motion.h2>
+        <motion.h1
+          initial={{ opacity: 0, x: -50 }}
+          animate={{
+            opacity: 1,
+            x: 0,
+            type: 'tween',
+            transition: { duration: 1, delay: 0.6 },
+          }}
+          className='pb-2 text-7xl font-bold'
+        >
+          Manjil Thapa.
+        </motion.h1>
+        <motion.p
+          initial={{ opacity: 0, x: -50 }}
+          animate={{
+            opacity: 1,
+            x: 0,
+            type: 'tween',
+            transition: { duration: 1, delay: 0.8 },
+          }}
+          className='text-xl mb-4 max-w-px'
+        >
+          Web Developer with a strong passion for frontend development. I specialize
+          in creating visually appealing, responsive, and feature-rich web
+          applications, prioritizing seamless user experience.
+        </motion.p>
+        <motion.p
+          initial={{ opacity: 0, x: -50 }}
+          animate={{
+            opacity: 1,
+            x: 0,
+            type: 'tween',
+            transition: { duration: 1, delay: 1 },
+          }}
+        >
+          Feel free to reach out - I am excited to connect!
+        </motion.p>
       </div>
-      <Image
-        src='/hero-placeholder.jpg'
-        alt='hero'
-        width={400}
-        height={400}
-        className='m-auto'
-      />
+      <motion.div
+        className='place-self-center'
+        initial={{ opacity: 0, x: 100 }}
+        animate={{
+          opacity: 1,
+          x: 0,
+          type: 'tween',
+          transition: { duration: 1, delay: 0.8 },
+        }}
+      >
+        <Image
+          src='/hero-placeholder.jpg'
+          alt='hero'
+          width={400}
+          height={400}
+          className='m-auto'
+        />
+      </motion.div>
     </section>
   );
 };
