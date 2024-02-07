375c1af2fe70156a19dfdbba4edbf378ed95f29b
Manjil Thapa <manjil.thapa.mgr@gmail.com>
2024-02-07 16:36:30 +1000

    fixed url issue

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,29 @@
-'use client';
-
 import Image from 'next/image';
-import Link from 'next/link';
 import { useState, useEffect, useRef } from 'react';
+import { motion } from 'framer-motion';
+
+const logoVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: { type: 'tween', duration: 1, ease: 'easeInOut' },
+  },
+};
+
+const listItemVariants = {
+  hidden: { opacity: 0, x: 100, y: -300 },
+  visible: index => ({
+    opacity: 1,
+    x: 0,
+    y: 0,
+    transition: {
+      delay: index * 0.2,
+      type: 'tween',
+      duration: 1,
+      ease: 'easeInOut',
+    },
+  }),
+};
 
 const Navbar = () => {
   const [navHidden, setNavHidden] = useState(false);
@@ -29,9 +50,7 @@ const Navbar = () => {
         }, 500);
       } else {
         setPadding('');
-        setBoxShadow(
-          '0px 4px 15px 0px #271A54, 0px 4px 15px 0px #2B1C5C, 0px 4px 15px 0px #2B1C5C'
-        );
+        setBoxShadow('0px 0px 40px -8px #232755,0px 8px 50px -4px #232755');
         setNavTransition(
           'transform 0.5s ease-in-out, padding 0.5s ease-in-out, box-shadow 0.5s ease-in-out'
         );
@@ -55,27 +74,40 @@ const Navbar = () => {
       }}
     >
       <nav
-        className={`flex justify-between w-full mx-auto my-2 max-w-screen-2xl py-2 px-8 rounded bg-[#2e2065e8]`}
+        className={`flex justify-between w-full mx-auto my-2 max-w-screen-2xl py-3 px-8 rounded bg-[#262a5cf1]`}
         style={{
           boxShadow: boxShadow,
         }}
       >
-        <h2>
-          <Link href='/'>
-            <Image src='/main-logo.svg' width={100} height={100} alt='logo' />
-          </Link>
-        </h2>
+        <motion.h2 initial='hidden' animate='visible' variants={logoVariants}>
+          <Image
+            src='/main-logo.svg'
+            width={100}
+            height={100}
+            alt='logo'
+            className='hover:bg-indigo-950 transition-colors duration-300 cursor-pointer'
+            onClick={() =>
+              document.getElementById('home'.replace('#', '')).scrollIntoView()
+            }
+          />
+        </motion.h2>
 
         <ul className='flex items-center gap-3 font-sora'>
-          <li>
-            <Link href='#projects'>Projects</Link>
-          </li>
-          <li>
-            <Link href='#about'>About</Link>
-          </li>
-          <li>
-            <Link href='#contact'>Contact</Link>
-          </li>
+          {navLinks.map((link, index) => (
+            <motion.li
+              key={index}
+              className='hover:text-indigo-200 tracking-wider px-1 cursor-pointer'
+              initial='hidden'
+              animate='visible'
+              variants={listItemVariants}
+              custom={index}
+              onClick={() =>
+                document.getElementById(link.href.replace('#', '')).scrollIntoView()
+              }
+            >
+              {link.name}
+            </motion.li>
+          ))}
         </ul>
       </nav>
     </div>
@@ -83,3 +115,9 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+const navLinks = [
+  { name: 'Projects', href: '#projects' },
+  { name: 'About', href: '#about' },
+  { name: 'Contact', href: '#contact' },
+];
